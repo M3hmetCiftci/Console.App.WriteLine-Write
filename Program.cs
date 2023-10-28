178d7e380d984eb52042da302499523cde28f73e
M3hmetCiftci <Mehmet.cift27@gmail.com>
2023-10-28 20:17:58 +0300

    Console.WriteLine-Write

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_WriteLine_Write
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+            Console.Read();
+
+            //Console.Write("Hello");
+            //Console.Write("Word");
+            //Console.Read();
+
+            //Console.WriteLine("HelloWord");
+            //Console.WriteLine("HwlloWord");
+            //Console.Read();
+
+
+            //Console.WriteLine("HelloWord");
+            //Console.Write("Hello");
+            //Console.Read();
+
+
+            //Console.WriteLine("HelloWord");
+            //Console.Write("Hello");
+            //Console.Write("Word");
+            //Console.Read();
+        }
+    }
+}
